b71b5fc89e52fe7399cda5fa082ae58d3757d6de
parshvJs <parshvsheth2006@gmail.com>
2025-03-17 21:31:19 +0530

    fix: update import statement for CreateAutomationForm to include file extension

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ import GenerateKnowledgeBase from "./pages/_knowledge/pages/GenerateKnowledgeBas
 import AddToExistingKnowledge from "./pages/_addToExisitingKnowledge/AddToExistingKnowledge"
 import AgenticAutomationLayout from "./pages/_AgenticAutomation/AgenticAutomationLayout"
 import AgenticAutomation from "./pages/_AgenticAutomation/pages/AgenticAutomation"
-import CreateAutomationForm from "./pages/_AgenticAutomation/pages/createNewAgenticAutomation"
+import CreateAutomationForm from "./pages/_AgenticAutomation/pages/CreateNewAgenticAutomation.jsx"
 import AgenticJobs from "./pages/_AgenticAutomation/pages/AgenticJobs"
 import JobDetails from "./pages/_AgenticAutomation/pages/JobDetails"
 
