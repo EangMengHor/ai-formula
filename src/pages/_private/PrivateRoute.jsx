79c07b0fca62c48ff7267048ed55f2502d6c576a
parshvJS <parshvsheth2006@gmail.com>
2025-05-21 22:59:15 +0530

    Refactor code structure for improved readability and maintainability

--- src/pages/_private/PrivateRoute.jsx
+++ src/pages/_private/PrivateRoute.jsx
@@ -4,65 +4,85 @@ import { useEffect, useState } from 'react';
 import { useToast } from '../../hooks/use-toast';
 import { useGetUserProfileStatus } from '../../hooks/use-get-user-profile-status';
 import { Loader2 } from 'lucide-react';
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription } from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
 
 export default function PrivateRoute() {
-	const { hasPersonalProfile, loading } = useGetUserProfileStatus();
-	const { user, setUser } = useUser();
-	const navigate = useNavigate();
-	const { toast } = useToast();
-	const location = useLocation();
-	const [isMounted, setIsMounted] = useState(false);
+    const { hasPersonalProfile, loading } = useGetUserProfileStatus();
+    const { user, setUser } = useUser();
+    const navigate = useNavigate();
+    const { toast } = useToast();
+    const location = useLocation();
+    const [isMounted, setIsMounted] = useState(false);
+    const [dialogOpen, setDialogOpen] = useState(false);
 
-	useEffect(() => {
-		setIsMounted(true);
-	}, []);
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
 
-	useEffect(() => {
-		if (!user.isAuthenticated) {
-			localStorage.getItem('id') &&
-				setUser({
-					id: localStorage.getItem('id'),
-					email: localStorage.getItem('email'),
-					isAuthenticated: true,
-				});
-			toast({
-				title: 'Access Not Allowed',
-				description: 'You are not authenticated! Please login to continue',
-				variant: 'default',
-			});
-			navigate('/login', { replace: true, state: { from: location } });
-		}
-	}, [user.isAuthenticated, navigate, location]);
-	useEffect(() => {
-		if (user.isAuthenticated) {
-			console.log('user is authenticated', location.pathname);
-			navigate(location.pathname);
-		}
-	}, [user.isAuthenticated]);
+    useEffect(() => {
+        if (!user.isAuthenticated) {
+            localStorage.getItem('id') &&
+                setUser({
+                    id: localStorage.getItem('id'),
+                    email: localStorage.getItem('email'),
+                    isAuthenticated: true,
+                });
+            toast({
+                title: 'Access Not Allowed',
+                description: 'You are not authenticated! Please login to continue',
+                variant: 'default',
+            });
+            navigate('/login', { replace: true, state: { from: location } });
+        }
+    }, [user.isAuthenticated, navigate, location, setUser, toast]);
 
-	useEffect(() => {
-		if (!isMounted) return;
-		if (!hasPersonalProfile && !loading) {
-			navigate('/a', { replace: true });
-		}
-		if (hasPersonalProfile && !loading && location.pathname === '/a') {
-			navigate('/', {
-				replace: true,
-			});
-		}
-	}, [hasPersonalProfile, navigate, loading, location.pathname]);
+    useEffect(() => {
+        if (user.isAuthenticated) {
+            navigate(location.pathname);
+        }
+    }, [user.isAuthenticated, navigate, location.pathname]);
 
-	if (loading) {
-		return (
-			<div className="flex items-center justify-center h-screen bg-black">
-				<Loader2 className="w-10 h-10 animate-spin text-white" />
-			</div>
-		);
-	}
+    useEffect(() => {
+        if (!isMounted) return;
+        if (!hasPersonalProfile && !loading) {
+            setDialogOpen(true);
+        } else {
+            setDialogOpen(false);
+        }
+    }, [hasPersonalProfile, loading, isMounted]);
 
-	return (
-		<div>
-			<Outlet />
-		</div>
-	);
+
+
+    return (
+        <>
+            <Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
+                <DialogContent className="w-[30%] bg-slate-800 flex flex-col gap-10">
+                    <DialogHeader>
+                        <div>
+                            <h3 className='text-white font-semibold text-xl py-2'>You Have Pending Profile Creation</h3>
+                        </div>
+                        <img src='/public/meshBg.png' alt='meshBg' className='h-60 border border-white rounded-md mt-5 ' />
+                    </DialogHeader>
+                    <div className='flex justify-between'>
+                        <ul className='list-disc  text-slate-300 text-lg ml-5'>
+                            <li>Takes 5 Min</li>
+                            <li>Personalized Experience <br /> With Ai</li>
+                        </ul>
+                        <ul className='list-disc  text-slate-300 text-lg ml-5'>
+                            <li>2-3 Daily Dips </li>
+                            <li>Portfolio Analysis</li>
+                        </ul>
+                    </div>
+                    <Button onClick={() => {
+                        navigate('/createPersonalProfileForm?isFirstTime=true', { state: { isFirstTime: true } });
+                        setDialogOpen(false);
+                    }}
+                        className="bg-slate-700 hover:bg-slate-600 px-4"
+                    >Create Profile</Button>
+                </DialogContent>
+            </Dialog>
+            <Outlet />
+        </>
+    );
 }
